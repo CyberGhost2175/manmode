84f377fa7f69dac9e73d958ff096c3c1d3959221
CyberGhost2175 <asanali.itstep@mail.ru>
2025-04-12 21:40:13 +0500

    make corrections

--- app/[locale]/checkout/[id]/payment-form.tsx
+++ app/[locale]/checkout/[id]/payment-form.tsx
@@ -1,226 +1,220 @@
 'use client'
 
 import {
-  PayPalButtons,
-  PayPalScriptProvider,
-  usePayPalScriptReducer,
+    PayPalButtons,
+    PayPalScriptProvider,
+    usePayPalScriptReducer,
 } from '@paypal/react-paypal-js'
-import { Card, CardContent } from '@/components/ui/card'
-import { useToast } from '@/hooks/use-toast'
+import {Card, CardContent} from '@/components/ui/card'
+import {useToast} from '@/hooks/use-toast'
 import {
-  approvePayPalOrder,
-  createPayPalOrder,
+    approvePayPalOrder,
+    createPayPalOrder,
 } from '@/lib/actions/order.actions'
-import { IOrder } from '@/lib/db/models/order.model'
-import { formatDateTime } from '@/lib/utils'
+import {IOrder} from '@/lib/db/models/order.model'
+import {formatDateTime} from '@/lib/utils'
 
 import CheckoutFooter from '../checkout-footer'
-import { redirect, useRouter } from 'next/navigation'
-import { Button } from '@/components/ui/button'
+import {redirect, useRouter} from 'next/navigation'
+import {Button} from '@/components/ui/button'
 import ProductPrice from '@/components/shared/product/product-price'
 import StripeForm from './stripe-form'
-import { Elements } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
+import {Elements} from '@stripe/react-stripe-js'
+import {loadStripe} from '@stripe/stripe-js'
+import {useTranslations} from "next-intl";
 
 const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
 )
 export default function OrderDetailsForm({
-  order,
-  paypalClientId,
-  clientSecret,
-}: {
-  order: IOrder
-  paypalClientId: string
-  isAdmin: boolean
-  clientSecret: string | null
+                                             order,
+                                             paypalClientId,
+                                             clientSecret,
+                                         }: {
+    order: IOrder
+    paypalClientId: string
+    isAdmin: boolean
+    clientSecret: string | null
 }) {
-  const router = useRouter()
-  const {
-    shippingAddress,
-    items,
-    itemsPrice,
-    taxPrice,
-    shippingPrice,
-    totalPrice,
-    paymentMethod,
-    expectedDeliveryDate,
-    isPaid,
-  } = order
-  const { toast } = useToast()
+    const router = useRouter()
+    const {
+        shippingAddress,
+        items,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentMethod,
+        expectedDeliveryDate,
+        isPaid,
+    } = order
+    const {toast} = useToast()
 
-  if (isPaid) {
-    redirect(`/account/orders/${order._id}`)
-  }
-  function PrintLoadingState() {
-    const [{ isPending, isRejected }] = usePayPalScriptReducer()
-    let status = ''
-    if (isPending) {
-      status = 'Loading PayPal...'
-    } else if (isRejected) {
-      status = 'Error in loading PayPal.'
+    if (isPaid) {
+        redirect(`/account/orders/${order._id}`)
     }
-    return status
-  }
-  const handleCreatePayPalOrder = async () => {
-    const res = await createPayPalOrder(order._id)
-    if (!res.success)
-      return toast({
-        description: res.message,
-        variant: 'destructive',
-      })
-    return res.data
-  }
-  const handleApprovePayPalOrder = async (data: { orderID: string }) => {
-    const res = await approvePayPalOrder(order._id, data)
-    toast({
-      description: res.message,
-      variant: res.success ? 'default' : 'destructive',
-    })
-  }
 
-  const CheckoutSummary = () => (
-    <Card>
-      <CardContent className='p-4'>
-        <div>
-          <div className='text-lg font-bold'>Order Summary</div>
-          <div className='space-y-2'>
-            <div className='flex justify-between'>
-              <span>Items:</span>
-              <span>
+    function PrintLoadingState() {
+        const [{isPending, isRejected}] = usePayPalScriptReducer()
+        let status = ''
+        if (isPending) {
+            status = 'Loading PayPal...'
+        } else if (isRejected) {
+            status = 'Error in loading PayPal.'
+        }
+        return status
+    }
+
+    const handleCreatePayPalOrder = async () => {
+        const res = await createPayPalOrder(order._id)
+        if (!res.success)
+            return toast({
+                description: res.message,
+                variant: 'destructive',
+            })
+        return res.data
+    }
+    const handleApprovePayPalOrder = async (data: { orderID: string }) => {
+        const res = await approvePayPalOrder(order._id, data)
+        toast({
+            description: res.message,
+            variant: res.success ? 'default' : 'destructive',
+        })
+    }
+
+    const CheckoutSummary = () => (
+        <Card>
+            <CardContent className='p-4'>
+                <div>
+                    <div className='text-lg font-bold'>{t('orderSummary')}</div>
+                    <div className='space-y-2'>
+                        <div className='flex justify-between'>
+                            <span>{t('items')}</span>
+                            <span>
                 {' '}
-                <ProductPrice price={itemsPrice} plain />
+                                <ProductPrice price={itemsPrice} plain/>
               </span>
-            </div>
-            <div className='flex justify-between'>
-              <span>Shipping & Handling:</span>
-              <span>
+                        </div>
+                        <div className='flex justify-between'>
+                            <span>{t('shippingHandling')}</span>
+                            <span>
                 {shippingPrice === undefined ? (
-                  '--'
+                    '--'
                 ) : shippingPrice === 0 ? (
-                  'FREE'
+                    'FREE'
                 ) : (
-                  <ProductPrice price={shippingPrice} plain />
+                    <ProductPrice price={shippingPrice} plain/>
                 )}
               </span>
-            </div>
-            <div className='flex justify-between'>
-              <span> Tax:</span>
-              <span>
+                        </div>
+                        <div className='flex justify-between'>
+                            <span> {t('tax')}</span>
+                            <span>
                 {taxPrice === undefined ? (
-                  '--'
+                    '--'
                 ) : (
-                  <ProductPrice price={taxPrice} plain />
+                    <ProductPrice price={taxPrice} plain/>
                 )}
               </span>
-            </div>
-            <div className='flex justify-between  pt-1 font-bold text-lg'>
-              <span> Order Total:</span>
-              <span>
+                        </div>
+                        <div className='flex justify-between  pt-1 font-bold text-lg'>
+                            <span> {t('orderTotal')}</span>
+                            <span>
                 {' '}
-                <ProductPrice price={totalPrice} plain />
+                                <ProductPrice price={totalPrice} plain/>
               </span>
-            </div>
+                        </div>
 
-            {!isPaid && paymentMethod === 'PayPal' && (
-              <div>
-                <PayPalScriptProvider options={{ clientId: paypalClientId }}>
-                  <PrintLoadingState />
-                  <PayPalButtons
-                    createOrder={handleCreatePayPalOrder}
-                    onApprove={handleApprovePayPalOrder}
-                  />
-                </PayPalScriptProvider>
-              </div>
-            )}
-            {!isPaid && paymentMethod === 'Stripe' && clientSecret && (
-              <Elements
-                options={{
-                  clientSecret,
-                }}
-                stripe={stripePromise}
-              >
-                <StripeForm
-                  priceInCents={Math.round(order.totalPrice * 100)}
-                  orderId={order._id}
-                />
-              </Elements>
-            )}
+                        {!isPaid && paymentMethod === 'PayPal' && (
+                            <div className="text-green-500">
+                                {t('inProcessing')}
+                            </div>
+                        )}
+                        {!isPaid && paymentMethod === 'Stripe' && clientSecret && (
+                            <div className="text-green-500">
+                                {t('inProcessing')}
+                            </div>
+                        )}
 
-            {!isPaid && paymentMethod === 'Cash On Delivery' && (
-              <Button
-                className='w-full rounded-full'
+                        {!isPaid && paymentMethod === 'Cash On Delivery' && (
+                            <Button
+                                className='w-full rounded-full'
+                                onClick={() => router.push(`/account/orders/${order._id}`)}
+                            >
+                                View Order
+                            </Button>
+                        )}
+
+                    </div>
+                </div>
+            </CardContent>
+            <Button
+                className=' rounded-full text-2xl m-2'
                 onClick={() => router.push(`/account/orders/${order._id}`)}
-              >
-                View Order
-              </Button>
-            )}
-          </div>
-        </div>
-      </CardContent>
-    </Card>
-  )
+            >⇦</Button>
+        </Card>
+    )
+    const t = useTranslations('Checkout')
+    return (
+        <main className='max-w-6xl mx-auto'>
+            <div className='grid md:grid-cols-4 gap-6'>
+                <div className='md:col-span-3'>
+                    {/* Shipping Address */}
+                    <div>
+                        <div className='grid md:grid-cols-3 my-3 pb-3'>
+                            <div className='text-lg font-bold'>
+                                <span>{t('shippingAddress')}</span>
+                            </div>
+                            <div className='col-span-2'>
+                                <p>
+                                    {shippingAddress.fullName} <br/>
+                                    {shippingAddress.street} <br/>
+                                    {`${shippingAddress.city}, ${shippingAddress.province}, ${shippingAddress.postalCode}, ${shippingAddress.country}`}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
-  return (
-    <main className='max-w-6xl mx-auto'>
-      <div className='grid md:grid-cols-4 gap-6'>
-        <div className='md:col-span-3'>
-          {/* Shipping Address */}
-          <div>
-            <div className='grid md:grid-cols-3 my-3 pb-3'>
-              <div className='text-lg font-bold'>
-                <span>Shipping Address</span>
-              </div>
-              <div className='col-span-2'>
-                <p>
-                  {shippingAddress.fullName} <br />
-                  {shippingAddress.street} <br />
-                  {`${shippingAddress.city}, ${shippingAddress.province}, ${shippingAddress.postalCode}, ${shippingAddress.country}`}
-                </p>
-              </div>
-            </div>
-          </div>
+                    {/* payment method */}
+                    <div className='border-y'>
+                        <div className='grid md:grid-cols-3 my-3 pb-3'>
+                            <div className='text-lg font-bold'>
+                                <span>{t('paymentMethod')}</span>
+                            </div>
+                            <div className='col-span-2'>
+                                <p>{paymentMethod}</p>
+                            </div>
+                        </div>
+                    </div>
 
-          {/* payment method */}
-          <div className='border-y'>
-            <div className='grid md:grid-cols-3 my-3 pb-3'>
-              <div className='text-lg font-bold'>
-                <span>Payment Method</span>
-              </div>
-              <div className='col-span-2'>
-                <p>{paymentMethod}</p>
-              </div>
-            </div>
-          </div>
+                    <div className='grid md:grid-cols-3 my-3 pb-3'>
+                        <div className='flex text-lg font-bold'>
+                            <span>{t('shipping')}</span>
+                        </div>
+                        <div className='col-span-2'>
+                            <p>
+                                {t('arriving')}:
+                                {formatDateTime(expectedDeliveryDate).dateOnly}
+                            </p>
+                            <ul>
+                                {items.map((item) => (
+                                    <li key={item.slug}>
+                                        {item.name} x {item.quantity} = {item.price}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='block md:hidden'>
+                        <CheckoutSummary/>
+                    </div>
 
-          <div className='grid md:grid-cols-3 my-3 pb-3'>
-            <div className='flex text-lg font-bold'>
-              <span>Items and shipping</span>
+                    <CheckoutFooter/>
+                </div>
+                <div className='hidden md:block'>
+                    <CheckoutSummary/>
+                </div>
             </div>
-            <div className='col-span-2'>
-              <p>
-                Delivery date:
-                {formatDateTime(expectedDeliveryDate).dateOnly}
-              </p>
-              <ul>
-                {items.map((item) => (
-                  <li key={item.slug}>
-                    {item.name} x {item.quantity} = {item.price}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className='block md:hidden'>
-            <CheckoutSummary />
-          </div>
-
-          <CheckoutFooter />
-        </div>
-        <div className='hidden md:block'>
-          <CheckoutSummary />
-        </div>
-      </div>
-    </main>
-  )
+        </main>
+    )
 }
